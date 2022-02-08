da40c329809dce8aedf6963db15083ba0aa3801d
Abbas Lamouri <abbas@Abbas-Omen.GENVAC.local>
2022-02-08 13:39:36 -0500

    M

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <script setup>
-// const data = await $fetch('/api/v1/categories', { method: 'POST', body: { id: 1 } })
-let data = []
+	// const data = await $fetch('/api/v1/categories', { method: 'POST', body: { id: 1 } })
+	let data = []
 
-try {
-  data = await $fetch('/api/v1/categories/61ff01356543ad8f41ddd649')
-  // console.log(data)
-} catch (error) {
-  console.log('BROWSER', error.data)
-}
+	try {
+		data = await $fetch('/api/v1/categories/61ff01236543ad8f41ddd645')
+		// console.log(data)
+	} catch (error) {
+		console.log('BROWSER', error.data)
+	}
 </script>
 
 <template>
-  <div>
-    Home
-    <pre>{{ data }}</pre>
-  </div>
+	<div>
+		Home
+		<pre>{{ data }}</pre>
+	</div>
 </template>
 
 <style lang=""></style>
--- server/api/v1/categories/index.js
+++ server/api/v1/categories/index.js
@@ -3,25 +3,30 @@ import { useBody, useQuery } from 'h3'
 import Category from '~/server/models/category'
 
 export default async (req, res) => {
-  try {
-    // xxx = 0
-    // throw new Error('My Error', 400)
-    // console.log(await useQuery(req))
-    // const body = await useBody(req)
-    // const params = await useQuery(req)
-    const docs = await Category.find()
-    // console.log('Body', body)
-    // console.log('params', params)
-    // return docs
-    // const doc = await Category.create(body)
-    // const doc = await Category.find()
-    // if (!doc) throw new Error(`We are not able to create a new document`, 404)
-    res.statusCode = 200
-    return docs
-  } catch (error) {
-    console.log('ERR', error.message)
-    res.statusCode = 400
-    res.end(error.message)
-    // return error
-  }
+	try {
+		// xxx = 0
+		// throw new Error('My Error', 400)
+		// console.log(await useQuery(req))
+		// const body = await useBody(req)
+		const params = await useQuery(req)
+		// console.log('Body', body)
+		console.log('params', params)
+		const id = req.url.split('/')[1]
+		console.log(id)
+		console.log('RM', req.method)
+		const docs = await Category.findById(id)
+
+		// return docs
+		// const doc = await Category.create(body)
+		// const doc = await Category.find()
+		// if (!doc) throw new Error(`We are not able to create a new document`, 404)
+		// const docs = await Category.find()
+		res.statusCode = 200
+		return docs
+	} catch (error) {
+		console.log('ERR', error.message)
+		res.statusCode = 400
+		res.end(error.message)
+		// return error
+	}
 }
